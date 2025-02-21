b799d3a8a8ffe7415df31f699d7ff9f1a6dcf312
officialjaykishan0001 <jaykishankharwar215@gmail.com>
2025-02-21 19:29:23 +0530

    user.controller modify with changed cookie setting

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -75,7 +75,7 @@ exports.login = async (req, res) => {
         };
 
         return res.status(200)
-            .cookie('token', token, { maxAge: 24 * 60 * 60 * 1000, httpOnly: true, sameSite: 'strict' }) // ✅ FIXED: `httpOnly`
+            .cookie('token', token, { maxAge: 24 * 60 * 60 * 1000, httpOnly: true, sameSite: 'none', secure: true }) 
             .json({ message: `Welcome back ${user.fullname}`, user, success: true });
 
     } catch (err) {
